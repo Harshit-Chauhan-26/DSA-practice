e62326fc9d6de37aa5850e1be6cebca7d4091885
Harshit Chauhan <businessharshit26@gmail.com>
2025-12-13 14:53:07 +0530

    Add files via upload

---
+++ Recursion/fibonacciSeries.java
@@ -0,0 +1,18 @@
+public class fibonacciSeries {
+    public static int fib(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 0) {
+            return 0;
+        }
+        int fnm1 = fib(n - 1);
+        int fnm2 = fib(n - 2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(fib(n));
+    }
+}
