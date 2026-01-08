b9ce502bf1c1b6e27790eca754406171619eaeb7
Harshit Chauhan <businessharshit26@gmail.com>
2026-01-08 07:08:21 +0530

    Add files via upload

---
+++ BackTracking/FindSubsets.java
@@ -0,0 +1,22 @@
+public class FindSubsets {
+    public static void Fsubset(String str, String ans, int i) {
+        // base case
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+        // yes choice
+        Fsubset(str, ans + str.charAt(i), i + 1);
+        // no choice
+        Fsubset(str, ans, i + 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        Fsubset(str, "", 0);
+    }
+}
